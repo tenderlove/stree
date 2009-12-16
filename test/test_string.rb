21aaa8fe941d4bafd1fcc42135545429dc77ebb6
Aaron Patterson <aaron.patterson@gmail.com>
2009-12-16 14:25:36 -0800

    test_square refactor

--- test/test_string.rb
+++ test/test_string.rb
@@ -12,12 +12,10 @@ class Stree
       str  = "foo"
       sstr = Stree::String.new(str)
 
-      if str.respond_to?(:getbyte)
-        0.upto(str.bytesize) do |i|
+      0.upto(str.bytesize) do |i|
+        if str.respond_to?(:getbyte)
           assert_equal str.getbyte(i), sstr[i], "failed on #{i}"
-        end
-      else
-        0.upto(str.bytesize) do |i|
+        else
           assert_equal str[i], sstr[i], "failed on #{i}"
         end
       end
